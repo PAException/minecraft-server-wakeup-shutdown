07a991e044ba7ffff3f93c6bd3358c580eb14221
Paul Huerkamp <huerkamp.paul@gmail.com>
2021-01-31 23:08:59 +0100

    C+CHcommunication

--- Client/src/main/java/io/github/paexception/mcsws/client/Client.java
+++ Client/src/main/java/io/github/paexception/mcsws/client/Client.java
@@ -9,6 +9,9 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
+import java.util.TimerTask;
+import java.util.UUID;
 
 public class Client {
 
@@ -31,6 +34,17 @@ public class Client {
 
 			String[] users = getMinecraftUser();
 
+
+			while (socket.isConnected()) {
+				String input = new String(socket.getInputStream().readAllBytes());
+				if (input.equalsIgnoreCase("await_reconnect")) {
+					System.out.println("Waiting for reconnection...");
+				}else if (input.equalsIgnoreCase("await_connection")) {
+					System.out.println("Waiting for connection...");
+				}else if (input.equalsIgnoreCase("no_permission")) {
+					System.out.println("You are not allowed to start the server!");
+				}
+			}
 			//name.uuid
 
 			//handle: await_reconnect,
@@ -45,15 +59,27 @@ public class Client {
 	}
 
 	private static String[] getMinecraftUser() {
+		String[] id = new String[0];
 		try {
-			FileInputStream fileInputStream = new FileInputStream(new File(System.getProperty(System.getProperty("user.home") + "\\AppData\\Roaming\\.minecraft\\usercache.json")));
+			FileInputStream fileInputStream = new FileInputStream(System.getProperty("user.home") + "\\AppData\\Roaming\\.minecraft\\usercache.json");
 			String string = new String(fileInputStream.readAllBytes());
+			fileInputStream.close();
+
+			id = string.split("},\\{");
+			for (int i = 0; i < id.length; i++) {
+				id[i] = id[i];
+				int indexs=id[i].indexOf("name\":\"") +7;//returns the index of is substring
+				int indexe=id[i].indexOf("\",\"");//returns the index of index substring
+				int indexs2=id[i].indexOf("uuid\":\"") +7;//returns the index of is substring
+				int indexe2=id[i].indexOf("\",\"",indexs2);//returns the index of index substring
+				id[i] = id[i].substring( indexs,  indexe) + "." + id[i].substring( indexs2,  indexe2);
+			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		return new String[0];
+		return id;
 	}
 
 }
